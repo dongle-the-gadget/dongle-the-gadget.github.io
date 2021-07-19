fa5c1e4d2b98590c9d7bf6feaf9c0268171ef698
Dongle <minh306@outlook.com.vn>
2021-07-19 14:46:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.write('Some links: <ul><li><a href="https://github.com/superkid200/">My GitHub profile</a></li></ul>');
+document.write('<p>Some links:</p> <ul><li><a href="https://github.com/superkid200/">My GitHub profile</a></li></ul>');
 
 // I'm inside a script, so let's have some fun.
 console.log('Hi! I' + "'" + 'in a script. Wanna join me? Head over to a mysterious place');
