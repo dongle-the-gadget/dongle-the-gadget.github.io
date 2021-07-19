7d58e29cecd025b81da430b20f659e341609c638
Dongle <minh306@outlook.com.vn>
2021-07-19 14:53:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.write('<p>Some links:</p> <ul><li><a href="https://github.com/superkid200/">My GitHub profile</a></li><li><a href="https://github.com/superkid200/superkid200.github.io">This page source code</a></li></ul>');
+document.write('<p>Some links:</p> <ul><li><a href="https://github.com/superkid200/">My GitHub profile</a></li><li><a href="https://github.com/superkid200/superkid200.github.io">This page source code</a></li><li><a href="https://twitter.com/a_donglee">My Twitter profile</a></li></ul>');
 
 // I'm inside a script, so let's have some fun.
 console.log('Hi! I' + "'" + 'm in a script. Wanna join me? Head over to a mysterious place');
