07923faab12b5ba453e63139c38c996b7b349580
Dongle <programcake@gmail.com>
2021-07-20 12:22:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.write('<p>GitHub life:</p> <ul><li><a href="https://github.com/superkid200/">My GitHub profile</a></li><li><a href="https://github.com/superkid200/superkid200.github.io">This page source code</a></li></ul>');
 
+document.write('<p>My email address: ' + '<' + 'a' + ' ' + 'h' + 'r' + 'e' + 'f' + '=' + '"' + 'm' + 'a' + 'i' + 'l' + 't' + 'o' + ':' + 'p' + 'r' + 'o' + 'g' + 'r' + 'a' + 'm' + 'c'+ 'a' + 'k' + 'e' + '@' + 'g' + 'm' + 'a' + 'i' + 'l' + '.' + 'c' + 'o' + 'm' + '"' + '>' + 'p' + 'r' + 'o' + 'g' + 'r' + 'a' + 'm' + 'c'+ 'a' + 'k' + 'e' + '@' + 'g' + 'm' + 'a' + 'i' + 'l' + '.' + 'c' + 'o' + 'm' + '<' + '/' + 'a' + '>' + '</p>');
+
 // I'm inside a script, so let's have some fun.
 console.log('Hi! I' + "'" + 'm in a script. Wanna join me? Head over to a mysterious place');
